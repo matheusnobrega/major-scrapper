26c80db543b376d4b6d545e19c6b943b42463826
matheusnobrega <matheus.nobrega.duarte@gmail.com>
2023-07-07 03:48:16 -0300

    Projeto finalizado

--- main2.py
+++ main2.py
@@ -6,8 +6,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import undetected_chromedriver as uc
+import pandas as pd
 
 BASE = 'https://www.hltv.org'
+info_jogadores = []
 
 options = uc.ChromeOptions()
 #options.add_argument("--headless")
@@ -32,8 +34,6 @@ tds = soup.findAll('td', {'class':'name-col'})
 
 majors = [td.find('a')['href'] for td in tds]
 
-print(majors)
-
 for link in majors:
     driver.get(BASE + link)
 
@@ -43,14 +43,29 @@ for link in majors:
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
+    camp = soup.find('div', {'class': 'event-hub-title'}).getText()
     tabela = soup.find('table', {'class': 'stats-table player-ratings-table'})
 
     trs = tabela.findAll('tr')
 
-    for tr in trs:
-        pass
+    for tr in trs[1:]:
+        jogador = {}
 
+        jogador['name'] = tr.find('td', {'class':'playerCol'}).getText()
+        jogador['nationality'] = tr.find('img', {'class':'flag'})['alt']
+        jogador['team'] = tr.find('td', {'class': 'teamCol'}).getText()
+        jogador['maps'] = int(tr.find('td', {'class': 'statsDetail'}).getText())
+        jogador['rounds'] = int(tr.find('td', {'class': 'statsDetail gtSmartphone-only'}).getText())
+        jogador['KD-diff'] = tr.find('td', {'class': 'kdDiffCol'}).getText()
+        jogador['KD'] = tr.findAll('td', {'class': 'statsDetail'})[-1].getText()
+        jogador['rating'] = tr.find('td', {'class': 'ratingCol'}).getText()
+        jogador['event'] = camp
 
+        info_jogadores.append(jogador)
+
+dataset = pd.DataFrame(info_jogadores)
+    
+dataset.to_csv('major-stats.csv', sep = ';', index = False, encoding = 'utf-8')
 
 
 driver.quit()
